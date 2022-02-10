d641ebcac0cc2ee7edf906b1d809f656987ad975
Ethan <61584967+Xrctius@users.noreply.github.com>
2022-02-10 19:39:05 +1100

    Finch Initial properties

--- src/finch.py
+++ src/finch.py
@@ -1,16 +1,17 @@
 class Finch:
-    lexome: bytearray = bytearray()
-    lexeme_step: int = 0
-
     def __init__(self,lexome: bytearray):
         self.lexome = lexome
-        self.lexeme_step = 0
-    
-    def inc(self):
-        if self.lexeme_step < len(self.lexome)-1:
-            self.lexeme_step = 0
-        else:
-            self.lexeme_step += 1
+        self.ax: bytearray = (0).to_bytes(4,'big')
+        self.bx: bytearray = (0).to_bytes(4,'big')
+        self.cx: bytearray = (0).to_bytes(4,'big')
+        self.s1: list[bytearray] = []
+        self.s2: list[bytearray] = []
+        self.read_h: int = 0
+        self.writ_h: int = 0
+        self.flow_h: int = 0 
+        self.inst_h: int = 0
+        self.input: list[bytearray] = [(0).to_bytes(4,'big'),(0).to_bytes(4,'big'),(0).to_bytes(4,'big')]
+        self.output: bytearray - (0).to_bytes(4,'big')
 
     def mut(self, index: int, replacement: int):
         self.lexome[index] = replacement
