b1f9d02fdf84d29c5fbda4ba35deb7b24de89bed
Ethan <61584967+Xrctius@users.noreply.github.com>
2022-02-13 22:59:02 +1100

    Cleanup Debug code

--- src/aviary.py
+++ src/aviary.py
@@ -5,15 +5,12 @@ from lexome import *
 def run_aviary(binary_lexome: list[bytearray], org_pops: list[int],binary_inst: bytearray, binary_dict: dict, str_dict: dict, size: int) -> None:
     # binary_dict   Key:lexemes Val:binary
     # str_dict      Key:binary  Val:lexemes
-
     finches: list[Finch] = []
     # Load finches
     for index, l in enumerate(binary_lexome):
         for i in range(org_pops[index]):
             finches.append(Finch(l))
-
     pretty("INFO","Innoculated aviary with starter finches")
-
     for i in range(1000):
         for finch in finches:
             if finch.skip_next_op:
@@ -21,11 +18,6 @@ def run_aviary(binary_lexome: list[bytearray], org_pops: list[int],binary_inst: 
                 finch.skip_next_op = False
             else:
                 run_op(str_dict,finch)
-
-def ind_typecheck(var, expected_type) -> bool:
-    if type(ind_typecheck) == expected_type:
-        return True
-    return False
 
 def replication_queue() -> None:
     pass
